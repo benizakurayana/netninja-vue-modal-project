9c971088702415ce5ec310f87b98769a3dcf4081
benizakurayana <122624742+benizakurayana@users.noreply.github.com>
2023-12-19 15:15:20 +0800

    This branch will store my tryouts to further understand how things work in Vue.

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <!-- <input type="text" ref="name">
+  <button @click="handleClick">click me</button> -->
+  <p>welcome...</p>
+  <div v-if="showModal">
+    <Modal aHardCodedAttribute="a hard coded attribute value" 
+      v-bind:header="modalHeader" :text="modalText" theme="sale" 
+      @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">nameed slot template1</a>
+      </template>
+      <template v-slot:links2>
+        <a href="#">nameed slot template2</a>
+      </template>
+      <p>default slot template given by this component, the parent componet to Modal.vue</p>
+    </Modal>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { Modal },
+  data() {
+    return {
+      title: 'My First Vue App :)',
+      modalHeader: 'Sign up for the giveaway!',
+      modalText: 'Grab your ninja swag for half price',
+      showModal: false,
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+
+      this.$data.title = 'aaaaa'
+      this.title = 'bbbbb'
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+
   }
 }
 </script>
@@ -22,5 +58,11 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" v-bind:class="{ sale: theme === 'sale', 
+        someClsss: true, anotherClass: aPropUndefined}">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
+            <slot>this won't show unless there's no default slot template specified in the parent component</slot>
+            <div>
+                <slot name="links2"></slot>
+            </div>
+            <div>
+                <slot name="links"></slot>
+            </div>
+            <p>{{ aHardCodedAttribute }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['aHardCodedAttribute', 'header', 'text', 'theme', 'aPropUndefined'],
+        methods: {
+            closeModal() {
+                this.$emit('close')
+            }
+        }
+    }
+</script>
+
+<style>
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top: 0;
+        position: fixed;
+        background: rgb(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1 {
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+    }
+    .modal p {
+        font-style: normal;
+    }
+    .modal.sale {
+        background: crimson;
+        color: white;
+    }
+    .modal.sale h1 {
+        color: white;
+    }
+</style>
